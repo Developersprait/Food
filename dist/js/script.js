60cc4fedeec8f003990bf03efaa8c8c6bab07f16
Leon Antoshyn <Doctorsprait@gmail.com>
2021-07-26 15:01:58 +0300

    async/await added

--- dist/js/script.js
+++ dist/js/script.js
@@ -163,36 +163,63 @@ document.addEventListener('DOMContentLoaded', () => {
             this.parent.append(element);
         };
     }
-    new Tab(
-        "img/tabs/vegy.jpg",
-        "vegy",
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        9,
-        '.menu .container'
 
-    ).render();
+    const getResourse = async (url) => {
+        const res = await fetch(url);
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status is ${res.status}`);
+        }
+        return res.json();
+    }
 
-    new Tab(
-        "img/tabs/elite.jpg",
-        "elite",
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        20,
-        '.menu .container',
-        'menu__item'
+    axios.get('http://localhost:3000/menu')
+        .then(data => {
+            data.data.forEach(({
+                img,
+                altimg,
+                title,
+                descr,
+                price
+            }) => {
+                new Tab(img, altimg, title, descr, price, '.menu .container').render();
+            });
+        });
+    // getResourse('http://localhost:3000/menu')
+    // .then(data =>{
 
-    ).render();
+    // });
+    // });
 
-    new Tab(
-        "img/tabs/post.jpg",
-        "post",
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-        15,
-        '.menu .container'
+    // new Tab(
+    //     "img/tabs/vegy.jpg",
+    //     "vegy",
+    //     'Меню "Фитнес"',
+    //     'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    //     9,
+    //     '.menu .container'
 
-    ).render();
+    // ).render();
+
+    // new Tab(
+    //     "img/tabs/elite.jpg",
+    //     "elite",
+    //     'Меню “Премиум”',
+    //     'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    //     20,
+    //     '.menu .container',
+    //     'menu__item'
+
+    // ).render();
+
+    // new Tab(
+    //     "img/tabs/post.jpg",
+    //     "post",
+    //     'Меню "Постное"',
+    //     'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+    //     15,
+    //     '.menu .container'
+
+    // ).render();
 
     //forms
     const forms = document.querySelectorAll('form');
@@ -204,10 +231,21 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     forms.forEach(item => {
-        postData(item)
+        bindPostData(item)
     });
 
-    function postData(form) {
+    const postData = async (url, data) => {
+        const res = await fetch(url, {
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: data
+        });
+        return res.json();
+    }
+
+    function bindPostData(form) {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
             const statusMessage = document.createElement('img');
@@ -225,31 +263,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
             const formData = new FormData(form);
 
-            const obj = {};
-            formData.forEach(function (value, key) {
-                obj[key] = value;
-            });
+            const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-            fetch('server.php', {
-                method: "POST",
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify(obj)
-            })
-            .then(data => data.text())
-            .then(data => {
-                console.log(data)
-                showThanksModal(message.success);
-                statusMessage.remove();
-                console.log('works');
-            })
-            .catch(() =>{
-                showThanksModal(message.fail);
-            })
-            .finally(() =>{
-                form.reset();
-            });
+
+            postData('  http://localhost:3000/requests', json)
+                .then(data => {
+                    console.log(data)
+                    showThanksModal(message.success);
+                    statusMessage.remove();
+                })
+                .catch(() => {
+                    showThanksModal(message.fail);
+                })
+                .finally(() => {
+                    form.reset();
+                });
 
             // request.addEventListener('load', () => {
             //     if (request.status === 200) {
@@ -289,6 +317,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     fetch('http://localhost:3000/menu')
-    .then(data => data.json())
-    .then(res => console.log(res))
+        .then(data => data.json())
+        .then(res => console.log(res))
 });
